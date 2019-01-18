7cb402879230f3f39fce245c087d10ecb30857ca
chrisliew <chriswvliew@gmail.com>
2019-01-19 03:34:32 +0700

    Updates

---
+++ precache-manifest.6b04446d9b0ed194858596b793c74a7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "790188b95d2ce1906d45",
+    "url": "/dev-portfolio/static/js/runtime~main.790188b9.js"
+  },
+  {
+    "revision": "7058d39f78e5cd3c97c8",
+    "url": "/dev-portfolio/static/js/main.7058d39f.chunk.js"
+  },
+  {
+    "revision": "e8e1044146f97a8b46f7",
+    "url": "/dev-portfolio/static/js/1.e8e10441.chunk.js"
+  },
+  {
+    "revision": "7058d39f78e5cd3c97c8",
+    "url": "/dev-portfolio/static/css/main.7cf6bdda.chunk.css"
+  },
+  {
+    "revision": "3db536bec2ab3de135d2b568538fb4d8",
+    "url": "/dev-portfolio/index.html"
+  }
+];
